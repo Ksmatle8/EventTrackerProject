acf9f543f6bb1a5dfbec636940e6e8f8bc00370e
hatle$ <kayla.hatle@yahoo.com>
2019-08-02 16:06:28 -0600

    Project Connected

--- JPANoNBucketList/src/test/java/com/skilldistillery/nonbucketlist/entities/ListTest.java
+++ JPANoNBucketList/src/test/java/com/skilldistillery/nonbucketlist/entities/ListTest.java
@@ -1,5 +1,56 @@
 package com.skilldistillery.nonbucketlist.entities;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 public class ListTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private List list;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("XtremePU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		list = em.find(List.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		list = null;
+	}
+
+	@Disabled
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test_Category_Mapping() {
+		assertEquals("skydiving", list.getId());
+		assertEquals(1, list.getId());
+	}
 
 }
---
+++ NonBucketList/src/main/java/com/skilldistillery/nonbucketlist/NonBucketListApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nonbucketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonBucketListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonBucketListApplication.class, args);
+	}
+
+}
